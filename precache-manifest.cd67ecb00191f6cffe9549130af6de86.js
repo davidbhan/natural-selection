9484abc645ef03140d107b69d6243d8bd4669c0c
davidbhan <davidhan321@gmail.com>
2020-05-01 23:46:42 +0800

    Updates

---
+++ precache-manifest.cd67ecb00191f6cffe9549130af6de86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9375e8ef7edf065b4699b6ea3839f2",
+    "url": "/natural-selection/index.html"
+  },
+  {
+    "revision": "2b66b7aa0a99645d15aa",
+    "url": "/natural-selection/static/css/main.6ca6c0ce.chunk.css"
+  },
+  {
+    "revision": "bea82f8f36fe64b4cede",
+    "url": "/natural-selection/static/js/2.234dd46c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/natural-selection/static/js/2.234dd46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b66b7aa0a99645d15aa",
+    "url": "/natural-selection/static/js/main.a9247dd1.chunk.js"
+  },
+  {
+    "revision": "1d15ac62254a2ad7f505",
+    "url": "/natural-selection/static/js/runtime-main.9a73d1fe.js"
+  }
+]);
